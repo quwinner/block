01292ce0f5672e67a4e82406416ea45af4b1dc93
Satamael <34512769+Satamael@users.noreply.github.com>
2019-03-06 17:27:41 +0400

    GiveMyHours

---
+++ UserControlAbout.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace block
+{
+    partial class UserControlAbout
+    {
+        /// <summary> 
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Обязательный метод для поддержки конструктора - не изменяйте 
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNickname = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonMore = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelNickname
+            // 
+            this.labelNickname.AutoSize = true;
+            this.labelNickname.Location = new System.Drawing.Point(6, 12);
+            this.labelNickname.Name = "labelNickname";
+            this.labelNickname.Size = new System.Drawing.Size(62, 13);
+            this.labelNickname.TabIndex = 0;
+            this.labelNickname.Text = "Погремуха";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(6, 37);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(105, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "мне 9 лет, 2 статьи";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonMore
+            // 
+            this.buttonMore.Location = new System.Drawing.Point(9, 109);
+            this.buttonMore.Name = "buttonMore";
+            this.buttonMore.Size = new System.Drawing.Size(75, 23);
+            this.buttonMore.TabIndex = 3;
+            this.buttonMore.Text = "Подробнее";
+            this.buttonMore.UseVisualStyleBackColor = true;
+            this.buttonMore.Click += new System.EventHandler(this.buttonMore_Click);
+            // 
+            // UserControlAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonMore);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.labelNickname);
+            this.Name = "UserControlAbout";
+            this.Size = new System.Drawing.Size(116, 144);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNickname;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonMore;
+    }
+}
