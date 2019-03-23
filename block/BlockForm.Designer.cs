05e895609e863261d92350d4b200cfce495e7ea1
Mikhail Abramov <beavisabra@yandex.ru>
2019-03-23 11:40:43 +0300

    Сохранение ЮзерКОнтролов на правильной форме

--- block/BlockForm.Designer.cs
+++ block/BlockForm.Designer.cs
@@ -137,7 +137,7 @@
             this.label5.Size = new System.Drawing.Size(35, 13);
             this.label5.TabIndex = 6;
             this.label5.Text = "label5";
-            this.label5.Click += new System.EventHandler(this.label4_Click);
+            this.label5.Click += new System.EventHandler(this.ArticleDetailsClick);
             // 
             // UCContextMenuStrip
             // 
